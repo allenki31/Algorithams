f0bd7311ffda9f92a89ae42cdda73de486be0b53
VINAY <VINAY@192.168.0.9>
2018-07-16 20:41:40 +0530

    1st file change

---
+++ src/com/app/Insertionsort.java
@@ -0,0 +1,43 @@
+package com.app;
+class Insertionsort
+{
+    
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i=1; i<n; ++i)
+        {
+            int key = arr[i];
+            int j = i-1;
+ 
+        
+            while (j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
+    }
+ 
+    
+    static void print(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+ 
+        System.out.println();
+    }
+ 
+    // Driver method
+    public static void main(String args[])
+    {        
+        int arr[] = {11,12,5,3,1};
+ 
+        Insertionsort ob = new Insertionsort();        
+        ob.sort(arr);
+         
+        print(arr);
+    }
+} 
